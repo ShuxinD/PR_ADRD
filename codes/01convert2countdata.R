03e6133fc94db6b73e7e8e46f178ae65900efd7d
ShuxinD <shuxind11@gmail.com>
2022-04-05 21:01:10 -0400

    create the codes to convert ADRD Medicare FFS admission record to the one-row-per-year-per-zipcode count data

---
+++ codes/01convert2countdata.R
@@ -0,0 +1,36 @@
+#' Project: particle radiation and ADRD admission in Medicare FFS
+#' Code: convert ADRD admission record from one-row-per-admission to one-row-per-zipcode count data
+#' Input: medicare FFS adrd admission record
+#' Output: "ADRD_zipcode_year.rds" as combination of zipcode and year
+#' Author: Shuxin Dong
+
+## 0. set up ----
+rm(list = ls())
+gc()
+
+setwd("/nfs/home/S/shd968/shared_space/ci3_shd968/PR_ADRD/")
+dir_ADRDadmission <- "/nfs/home/S/shd968/shared_space/ci3_analysis/data_ADRDhospitalization/ADRDhospitalization_CCWlist/"
+dir_dataout <- "/nfs/home/S/shd968/shared_space/ci3_shd968/PR_ADRD/data/"
+
+library(data.table)
+library(fst)
+# library(tigris)
+# library(sf)
+
+## 1. load Medicare FFS admission record ----
+ADRDhosp <- NULL
+for (i in 2000:2016) {
+  adm_ <- read_fst(paste0(dir_ADRDadmission, "ADRD_", i,".fst"), as.data.table = T)
+  ADRDhosp <- rbind(ADRDhosp, adm_)
+  print(paste0("finish loading file:", "ADRD_", i,".fst"))
+}
+rm(adm_)
+gc()
+ADRDhosp <- unique(ADRDhosp)
+names(ADRDhosp)
+ADRDhosp[,year:=year(ADATE)]
+
+## 2. aggregate to count data and save ----
+count_dt <- ADRDhosp[,.N,by=c("year","zipcode_R")]
+
+saveRDS(count_dt, paste0(dir_dataout, "ADRD_zipcode_year.rds"))
