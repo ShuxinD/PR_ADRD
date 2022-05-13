ca45ef65f7d66d87a960cac46ac1bbc7084ae5a4
ShuxinD <shuxind11@gmail.com>
2022-05-13 02:09:39 -0400

    rename and re-merge all datasets

--- codes/04mergeDatasetsALL.R
+++ codes/04mergeDatasetsALL.R
@@ -13,7 +13,6 @@ dir_data <- "/nfs/home/S/shd968/shared_space/ci3_shd968/PR_ADRD/data/"
 
 library(data.table)
 library(fst)
-library(NSAPHutils)
 
 int_to_string_zip <- function(zipcode_number){
   zip_raw <- as.character(zipcode_number)
@@ -27,17 +26,41 @@ int_to_string_zip <- function(zipcode_number){
 ## 1. load datasets -------------------------------------
 ADRD_count <- readRDS(paste0(dir_data,"ADRD_zipcode_year.rds"))
 covariates <- read_fst(paste0(dir_data, "all_covariates.fst"))
-beta <- read.csv(paste0(dir_data, "zipbeta_2001_2017.csv"))
+pop_count <- readRDS(paste0(dir_data,"medicare_enrollee_count.rds"))
+load(paste0(dir_data,"zip_annual_SD.RData"))
+beta <- zip_annual
+rm(zip_annual)
+gc()
 
 head(ADRD_count)
 ADRD_count[,zipcode_R:=int_to_string_zip(zipcode_R)]
+names(ADRD_count)
+names(ADRD_count) <- c("year", "zipcode", "ADRDhosp")
+
 head(covariates)
+names(covariates)[1:2] <- c("zipcode", "year")
+
 head(beta,20)
 setDT(beta)
-beta[,`:=`(X=NULL, zip=int_to_string_zip(zip))]
+beta[,`:=`(State=NULL, ZIPCODE=int_to_string_zip(ZIPCODE))]
+names(beta) <- c("zipcode", "beta", "year")
+
+head(pop_count)
+names(pop_count) <- c("year", "zipcode", "p_count")
 
 ## 2. merge all datasets together ------
-dt <- merge(ADRD_count, beta, by.x = c("year", "zipcode_R"), by.y = c("year", "zip"))
-dt <- merge(dt, covariates, by.x = c("year", "zipcode_R"), by.y = c("year", "zip"))
+dt <- merge(pop_count, ADRD_count, by = c("year", "zipcode"), all.x = T)
+dt[is.na(dt)] <- 0
+
+dt <- merge(dt, covariates, by = c("year", "zipcode"), all.x = T)
+
+dt[,year_prev:=year-1][]
+dt <- merge(dt, beta, by.x = c("year_prev", "zipcode"), by.y = c("year","zipcode"), all.x = T)
+
+dt <- dt[year>=2002 & year <=2016,]
+
+dt # 613785
+
+na.omit(dt) # 379531
 
 write_fst(na.omit(dt), paste0(dir_data, "final_dt.fst"))
