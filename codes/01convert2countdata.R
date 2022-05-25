6e395ccd04a073d69cd98118ed6eaf4144fce8cc
ShuxinD <shuxind11@gmail.com>
2022-05-24 21:38:19 -0400

    fix zipcode in aggregated hospitalization data

--- codes/01convert2countdata.R
+++ codes/01convert2countdata.R
@@ -14,17 +14,7 @@ dir_dataout <- "/nfs/home/S/shd968/shared_space/ci3_shd968/PR_ADRD/data/"
 
 library(data.table)
 library(fst)
-# library(tigris)
-# library(sf)
-
-int_to_string_zip <- function(zipcode_number){
-  zip_raw <- as.character(zipcode_number)
-  zip_raw <- ifelse(nchar(zip_raw)<5, paste0("0",zip_raw), zip_raw)
-  zip_raw <- ifelse(nchar(zip_raw)<5, paste0("0",zip_raw), zip_raw)
-  zip_raw <- ifelse(nchar(zip_raw)<5, paste0("0",zip_raw), zip_raw)
-  zip_raw <- ifelse(nchar(zip_raw)<5, paste0("0",zip_raw), zip_raw)
-  return(zip_raw)
-}
+library(zipcodeR)
 
 ## 1. load Medicare FFS admission record ----
 ADRDhosp <- NULL
@@ -36,8 +26,14 @@ for (i in 2000:2016) {
 rm(adm_)
 gc()
 ADRDhosp <- unique(ADRDhosp)
-names(ADRDhosp)
+colnames(ADRDhosp)
+# [1] "QID"            "ADATE"          "DDATE"          "zipcode_R"      "DIAG1"          "DIAG2"         
+# [7] "DIAG3"          "DIAG4"          "DIAG5"          "DIAG6"          "DIAG7"          "DIAG8"         
+# [13] "DIAG9"          "DIAG10"         "AGE"            "Sex_gp"         "Race_gp"        "SSA_STATE_CD"  
+# [19] "SSA_CNTY_CD"    "PROV_NUM"       "ADM_SOURCE"     "ADM_TYPE"       "Dual"           "year"          
+# [25] "AD_primary"     "AD_any"         "AD_secondary"   "ADRD_primary"   "ADRD_any"       "ADRD_secondary"
 ADRDhosp[,year:=year(ADATE)]
+ADRDhosp[,zipcode_R:=normalize_zip(zipcode_R)]
 
 ## 2. aggregate to count data and save ----
 count_dt <- ADRDhosp[,.N,by=c("year","zipcode_R")]
